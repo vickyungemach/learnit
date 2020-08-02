8e2fbc06a3ba52f38b27c173c31e9eae98deebc7
vicky <vicky.ungemach95@gmail.com>
2020-08-02 12:36:05 -0700

    added production build parameters

--- server.js
+++ server.js
@@ -13,14 +13,6 @@ app.use(express.json());
 // Enable CORS
 app.use(cors());
 
-// Set react as static page
-// app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-// app.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname,'client', 'build', 'index.html'));
-// });
-
-
 // Mount routers
 app.use('/api/words', require('./routes/words'));
 app.use('/api/lists', require('./routes/lists'));
@@ -29,5 +21,24 @@ app.use('/api/user', require('./routes/auth'));
 app.get('/', (req, res) => {
     res.send('Hello Express');
 })
+
+
+/* ==========================================
+   Prduction build
+========================================== */
+
+/* 
+    // Mount routers
+    app.use('/api/words', require('./routes/words'));
+    app.use('/api/lists', require('./routes/lists'));
+    app.use('/api/user', require('./routes/auth'));
+
+
+    // Set react as static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    }); 
+*/
 
 app.listen(PORT, console.log('Server running on ' + PORT));
