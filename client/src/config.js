f223579f72595c1ce33b8d1c5b9a39ddb356c9ca
Vicky Ungemach <vicky.ungemach@gmail.com>
2020-10-15 14:57:55 -0700

    changed config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const url = 'http://localhost:5000';
-// export const url = 'http://www.learnit.ltd';
+// export const url = 'http://localhost:5000';
+export const url = 'http://www.learnit.ltd';
