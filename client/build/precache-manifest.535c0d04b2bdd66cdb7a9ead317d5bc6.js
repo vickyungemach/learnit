bc6568f9fb5010355a1d228cee7b6f08198d41a6
Vicky Ungemach <vicky.ungemach@gmail.com>
2020-10-15 13:23:41 -0700

    fixing flashing login page

---
+++ client/build/precache-manifest.535c0d04b2bdd66cdb7a9ead317d5bc6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac463fe0338a1c906adbab3fa895cebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdc9437be78f05dae93e",
+    "url": "/static/css/main.50bc0352.chunk.css"
+  },
+  {
+    "revision": "27fe88e08c868057dd3a",
+    "url": "/static/js/2.331f5195.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.331f5195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1376d579de6b3dd1aacb",
+    "url": "/static/js/3.0bbf829c.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/static/js/3.0bbf829c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdc9437be78f05dae93e",
+    "url": "/static/js/main.53fbddf5.chunk.js"
+  },
+  {
+    "revision": "114ed0352d0188d692f6",
+    "url": "/static/js/runtime-main.3e7a1c8b.js"
+  },
+  {
+    "revision": "3c410f318d7fb2004246244c86c3aec8",
+    "url": "/static/media/bonus-points.3c410f31.mp3"
+  },
+  {
+    "revision": "53aaa815ac956ded66e60b385ef58e70",
+    "url": "/static/media/gold-circle.53aaa815.png"
+  },
+  {
+    "revision": "f4a2f72770ad4fed9b7a0aa031acf12b",
+    "url": "/static/media/pineapple.f4a2f727.png"
+  },
+  {
+    "revision": "5e6db339d650761c332739458e321c9e",
+    "url": "/static/media/silver-circle.5e6db339.png"
+  }
+]);
