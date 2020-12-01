b5e52fe9d4ed7616d3062b0e96c97ed627322775
Vicky Ungemach <vicky.ungemach@gmail.com>
2020-12-01 11:37:03 -0800

    Reset word rating when wrong answer

--- client/src/components/pages/home/Review.js
+++ client/src/components/pages/home/Review.js
@@ -134,7 +134,7 @@ const Review = ({ review: { list, currentWord, translation, id, rating, loadingL
                     // Subtract from rating
                     if (rating !== 0) {
                         wordUpdate(id, {
-                            rating: rating - 1
+                            rating: 0
                         });
                     }
 
